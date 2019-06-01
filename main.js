cb59642f3c0ea2bcc2b7c058c7805a020b83a1f6
Jordan Epps <jordanepps@gmail.com>
2019-06-01 11:31:43 -0400

    Complete understanding quicksort

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Given the following list of numbers
 const numList = [21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40];
 
 function mergeSort(array) {
@@ -11,7 +12,6 @@ function mergeSort(array) {
 
     left = mergeSort(left);
     right = mergeSort(right);
-    console.log(left, right);
     return merge(left, right, array);
 };
 
@@ -37,7 +37,7 @@ function merge(left, right, array) {
     return array;
 };
 
-console.log(mergeSort(numList));
+mergeSort(numList);
 
 // What is the resulting list that will be 
 // sorted after 3 recursive calls to mergesort?
@@ -48,3 +48,73 @@ console.log(mergeSort(numList));
 // [ 1, 2, 9, 16, 21, 26, 27, 28, 29, 34, 39, 40, 43, 45, 46, 49 ]
 
 // What are the first 2 lists to be merged?
+// [ 21 ] && [ 1 ]
+
+// Which two lists would be merged on the 7th merge?
+// [ 1, 21, 26, 45 ] && [ 2, 9, 28, 29 ]
+
+
+function swap(array, i, j) {
+    const tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
+};
+
+function bubbleSort(array) {
+    let swaps = 0;
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+            swap(array, i, i + 1);
+            swaps++;
+        }
+    }
+
+    if (swaps > 0) {
+        return bubbleSort(array);
+    }
+    return array;
+};
+
+function quickSort(array, start = 0, end = array.length) {
+    if (start >= end) {
+        return array;
+    }
+
+    const middle = partition(array, start, end);
+    array = quickSort(array, start, middle, );
+    array = quickSort(array, middle + 1, end);
+
+    return array;
+};
+
+function partition(array, start, end) {
+    const pivot = array[end - 1];
+    let j = start;
+    for (let i = start; i < end - 1; i++) {
+        if (array[i] <= pivot) {
+            swap(array, i, j);
+            j++;
+        }
+    }
+    swap(array, end - 1, j);
+    return j;
+};
+
+// 1.)Suppose you are debugging a quicksort implementation that is supposed 
+// to sort an array in ascending order. After the first partition step has 
+// been completed, the contents of the array is in the 
+// following order: 3 9 1 14 17 24 22 20. 
+// Which of the following statements is correct about the partition step? 
+// Explain your answer.
+
+//The pivot could have been either 14 or 17 because the values that are less than
+//pivot go to one half of the array and the values greater than the pivot go to the
+//other half
+
+const quickNumList = [14, 17, 13, 15, 19, 10, 3, 16, 9, 12];
+// 2.)Given the following list of numbers 14, 17, 13, 15, 19, 10, 3, 16, 9, 12 show the 
+// resulting list after the second partitioning according to the quicksort algorithm.
+
+//[ 3, 9, 10, 12, 19, 14, 17, 16, 13, 15 ]
+
+quickSort(quickNumList);
